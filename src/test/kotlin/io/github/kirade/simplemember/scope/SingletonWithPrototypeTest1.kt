2a89fc792304645eb2166c1e62c3988d4e28fd2f
JaeyoungHeo <jay.jaeyoung@gmail.com>
2024-05-12 22:44:46 +0900

    feat: singleton bean uses prototype bean

---
+++ src/test/kotlin/io/github/kirade/simplemember/scope/SingletonWithPrototypeTest1.kt
@@ -0,0 +1,66 @@
+package io.github.kirade.simplemember.scope
+
+import jakarta.annotation.PostConstruct
+import jakarta.annotation.PreDestroy
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.springframework.context.annotation.AnnotationConfigApplicationContext
+import org.springframework.context.annotation.Scope
+
+class SingletonWithPrototypeTest1 {
+
+    @Scope("singleton")
+    class ClientBean(private val prototypeBean: PrototypeBean) {
+
+        fun logic(): Int{
+            prototypeBean.addCount()
+            return prototypeBean.count
+        }
+    }
+
+    @Scope("prototype")
+    class PrototypeBean {
+        var count: Int = 0
+
+        fun addCount() {
+            count++
+        }
+
+        @PostConstruct
+        fun init(){
+            println("PrototypeBean.init $this")
+        }
+
+        @PreDestroy
+        fun destroy() {
+            println("PrototypeBean.destroy")
+        }
+    }
+
+    @Test
+    fun prototypeFind() {
+        val ac = AnnotationConfigApplicationContext(PrototypeBean::class.java)
+
+        val prototypeBean1 = ac.getBean(PrototypeBean::class.java)
+        prototypeBean1.addCount()
+        assertThat(prototypeBean1.count).isEqualTo(1)
+
+        val prototypeBean2 = ac.getBean(PrototypeBean::class.java)
+        prototypeBean2.addCount()
+        assertThat(prototypeBean2.count).isEqualTo(1)
+    }
+
+    @Test
+    fun singletonClientUsePrototype() {
+        val ac = AnnotationConfigApplicationContext(ClientBean::class.java, PrototypeBean::class.java)
+
+        val clientBean1 = ac.getBean(ClientBean::class.java)
+        val count1 = clientBean1.logic()
+        val clientBean2 = ac.getBean(ClientBean::class.java)
+        val count2 = clientBean2.logic()
+
+        assertThat(count1).isEqualTo(1)
+        assertThat(count2).isEqualTo(2)
+
+    }
+}
